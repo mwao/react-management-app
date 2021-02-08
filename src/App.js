8e10f3b76a43c949fc25bea3856cb6ad22cc931d
mwao <vldzmwjdgus95@gmail.com>
2021-02-08 15:17:40 +0900

    material-ui (scroll did not work..)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './component/Customer';
 import Customer from './component/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles=(theme)=>({
+  root:{
+    width:'100%',
+    marginTop:theme.spacing(3),
+    overflowX:'auto'
+  },
+  table:{
+    minWidth:1080
+  },
+})
+
 const customers=[{
   'id':1,
   'image':'https://placeimg.com/64/64/1',
@@ -27,19 +47,39 @@ const customers=[{
 }
 ]
 
-function App() {
+class App extends React.Component{
+  render(){
+    const {classes}=this.props;
   return (
-    customers.map(customers=>
+    <div>
+      <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableCell>번호</TableCell>
+          <TableCell>이미지</TableCell>
+          <TableCell>이름</TableCell>
+          <TableCell>생년월일</TableCell>
+          <TableCell>성별</TableCell>
+          <TableCell>직업</TableCell>
+
+        </TableHead>
+        <TableBody>
+{customers.map(customers=>{return(
       <Customer 
    name={customers.name}
    birthday={customers.birthday}
    gender={customers.gender}
    job={customers.job}
    id={customers.id}
-   image={customers.image}></Customer>
-      )
-   
+   image={customers.image}></Customer>)
+     } )}
+        </TableBody>
+      </Table>
+      </Paper>
+    </div>
   );
+  }
 }
 
-export default App;
+
+export default withStyles(styles)(App);
